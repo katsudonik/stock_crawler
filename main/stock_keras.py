76577baff8541a603b7b96575691459ef66cf737
kaikeda <katsudonwork@gmail.com>
2017-05-04 23:23:46 +0900

    press_release_checker

--- main/stock_keras.py
+++ main/stock_keras.py
@@ -29,7 +29,7 @@ def build_model(in_shape):
     model = Sequential()
     model.add(Conv2D(32, (3, 3),
     padding='same',
-	input_shape=(3,50,50)))
+	input_shape=(119,50,50,3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
