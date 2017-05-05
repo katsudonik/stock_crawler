5e48bc58e13f9743a2ec687cc204e8bc11581ab4
kaikeda <katsudonwork@gmail.com>
2017-05-05 20:21:03 +0900

    press_release_checker

--- main/stock_keras.py
+++ main/stock_keras.py
@@ -3,27 +3,25 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.utils import np_utils
 import numpy as np
+import yaml
 
-# 分類対象のカテゴリ
-root_dir = "./image/"
-categories = ["up_signal", "high_update", "after_up", "down_signal", "other"]
-nb_classes = len(categories)
-image_size = 128
-img_height = image_size #画像の縦size(px)
-img_width = image_size #画像の横size(px)
-img_channels = 3 #カラー画像:3　※白黒の場合は1
+### functions ###########################################################################
+def load_config(path):
+    f = open(path, 'r')
+    config = yaml.load(f)
+    f.close()
+    return config
 
-def main():
+def load_data(quantified_data_path):
+    data = {}
     # 分類対象画像を数値化したデータをロード(result of stock-makedata.py) --- (※1)
-    X_train, X_test, y_train, y_test = np.load("./image/stock.npy")
+    X_train, X_test, y_train, y_test = np.load(quantified_data_path)
     # データを0-1に正規化する
-    X_train = X_train.astype("float") / 256 #色は最大で256画素->256で割れば0-1になる
-    X_test  = X_test.astype("float")  / 256
-    y_train = np_utils.to_categorical(y_train, nb_classes) #transform to one-hot encoding
-    y_test  = np_utils.to_categorical(y_test, nb_classes)
-    # モデルを訓練し評価する
-    model = model_train(X_train, y_train)
-    model_eval(model, X_test, y_test)
+    data['X_train'] = X_train.astype("float") / 256 #色は最大で256画素->256で割れば0-1になる
+    data['X_test']  = X_test.astype("float")  / 256
+    data['y_train'] = np_utils.to_categorical(y_train, len(config['categories'])) #transform to one-hot encoding
+    data['y_test']  = np_utils.to_categorical(y_test, len(config['categories']))
+    return data
 
 # モデルを構築 --- (※2)
 def build_model():
@@ -31,7 +29,7 @@ def build_model():
     #keras2->Conv2D
     model.add(Conv2D(32, (3, 3),#filter(convolution window/kernel):3*3size, 32 sheet
     padding='same',
-	input_shape=(img_height, img_width, img_channels))) #channels_last #first layer in a model, provide input_shape
+	input_shape=(config['categories']['height'], config['categories']['width'], config['categories']['channels']))) #channels_last #first layer in a model, provide input_shape
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2))) #2*2に圧縮
     model.add(Dropout(0.25))
@@ -44,7 +42,7 @@ def build_model():
     model.add(Dense(512))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(nb_classes))
+    model.add(Dense(len(config['categories'])))
     model.add(Activation('softmax'))
     model.compile(loss='binary_crossentropy',
 	optimizer='rmsprop',
@@ -56,13 +54,10 @@ def model_train(X_train, y_train):
     model = build_model()
     #データを与えて学習させる
     model.fit(X_train, y_train, batch_size=32, nb_epoch=30)
-    # 学習したモデルを保存する --- (※4)->保存したモデルは再利用できる
     # モデルを保存
-    model_json_str = model.to_json()
-    open('stock_model.json', 'w').write(model_json_str)
+    open(config['model_file'], 'w').write(model.to_json())
     # 学習結果を保存
-    hdf5_file = "./image/stock-model.hdf5"
-    model.save_weights(hdf5_file)
+    model.save_weights(config['weights_file'])
     return model
 
 # モデルを評価する --- (※5)
@@ -71,6 +66,14 @@ def model_eval(model, X_test, y_test):
     print('loss=', score[0])
     print('accuracy=', score[1])
 
+def main():
+    data = load_data(DATA_FILE)
+    model = model_train(data['X_train'], data['y_train'])
+    model_eval(model, data['X_test'], data['y_test'])
+
+### execute ###################################################################
+config = load_config('./stock_keras_config.yml')
+DATA_FILE = config['img_dir'] + "/stock.npy"
 if __name__ == "__main__":
     main()
 
--- main/stock_keras_load.py
+++ main/stock_keras_load.py
@@ -2,41 +2,45 @@ from keras.utils import np_utils
 import numpy as np
 from keras.models import model_from_json
 
-# 分類対象のカテゴリ
-root_dir = "./image/"
-categories = ["up_signal", "high_update", "after_up", "down_signal", "other"]
-nb_classes = len(categories)
-image_size = 128
-img_height = image_size #画像の縦size(px)
-img_width = image_size #画像の横size(px)
-img_channels = 3 #カラー画像:3　※白黒の場合は1
+### functions ###########################################################################
+def load_config(path):
+    f = open(path, 'r')
+    config = yaml.load(f)
+    f.close()
+    return config
 
-def main():
+def load_data(quantified_data_path):
+    data = {}
     # 分類対象画像を数値化したデータをロード(result of stock-makedata.py) --- (※1)
-    X_train, X_test, y_train, y_test = np.load("./image/stock.npy")
+    X_train, X_test, y_train, y_test = np.load(quantified_data_path)
     # データを0-1に正規化する
-    X_train = X_train.astype("float") / 256 #色は最大で256画素->256で割れば0-1になる
-    X_test  = X_test.astype("float")  / 256
-    y_train = np_utils.to_categorical(y_train, nb_classes) #transform to one-hot encoding
-    y_test  = np_utils.to_categorical(y_test, nb_classes)
+    data['X_train'] = X_train.astype("float") / 256 #色は最大で256画素->256で割れば0-1になる
+    data['X_test']  = X_test.astype("float")  / 256
+    data['y_train'] = np_utils.to_categorical(y_train, len(config['categories'])) #transform to one-hot encoding
+    data['y_test']  = np_utils.to_categorical(y_test, len(config['categories']))
+    return data
 
-    # モデルを読み込む
-    model = model_from_json(open('stock_model.json').read())
-    # 学習結果を読み込む
-    model.load_weights('./image/stock-model.hdf5')
-    model.summary();
-    model.compile(loss='binary_crossentropy',
-                  optimizer='rmsprop',
-                  metrics=['accuracy'])
-
-    model_eval(model, X_test, y_test)
-
-# モデルを評価する --- (※5)
 def model_eval(model, X_test, y_test):
     score = model.evaluate(X_test, y_test)
     print('loss=', score[0])
     print('accuracy=', score[1])
 
+def main():
+    # モデルを読み込む
+    model = model_from_json(open(config['model_file']).read())
+    # 学習結果を読み込む
+    model.load_weights(config['weights_file'])
+    model.summary();
+    model.compile(loss='binary_crossentropy',
+                  optimizer='rmsprop',
+                  metrics=['accuracy'])
+
+    data = load_data(DATA_FILE)
+    model_eval(model, data['X_test'], data['y_test'])
+
+### execute ###################################################################
+config = load_config('./stock_keras_config.yml')
+DATA_FILE = config['img_dir'] + "/stock.npy"
 if __name__ == "__main__":
     main()
 
