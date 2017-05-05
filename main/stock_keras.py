16f1563aaa04381d4e786aca5e0748baf67b9d18
kaikeda <katsudonwork@gmail.com>
2017-05-05 19:31:54 +0900

    press_release_checker

--- main/stock_keras.py
+++ main/stock_keras.py
@@ -8,30 +8,32 @@ import numpy as np
 root_dir = "./image/"
 categories = ["up_signal", "high_update", "after_up", "down_signal", "other"]
 nb_classes = len(categories)
-image_size = 50
+image_size = 128
+img_height = image_size #画像の縦size(px)
+img_width = image_size #画像の横size(px)
+img_channels = 3 #カラー画像:3　※白黒の場合は1
 
-# データをロード --- (※1)
 def main():
+    # 分類対象画像を数値化したデータをロード(result of stock-makedata.py) --- (※1)
     X_train, X_test, y_train, y_test = np.load("./image/stock.npy")
-    # データを正規化する
-    X_train = X_train.astype("float") / 256
+    # データを0-1に正規化する
+    X_train = X_train.astype("float") / 256 #色は最大で256画素->256で割れば0-1になる
     X_test  = X_test.astype("float")  / 256
-    y_train = np_utils.to_categorical(y_train, nb_classes)
+    y_train = np_utils.to_categorical(y_train, nb_classes) #transform to one-hot encoding
     y_test  = np_utils.to_categorical(y_test, nb_classes)
     # モデルを訓練し評価する
-    # X_train = X_train.reshape((X_train.shape[0], 50, 50, 1))
-    # X_test = X_test.reshape((X_test.shape[0], 50, 50, 1))
     model = model_train(X_train, y_train)
     model_eval(model, X_test, y_test)
 
 # モデルを構築 --- (※2)
-def build_model(in_shape):
+def build_model():
     model = Sequential()
-    model.add(Conv2D(32, (3, 3),
+    #keras2->Conv2D
+    model.add(Conv2D(32, (3, 3),#filter(convolution window/kernel):3*3size, 32 sheet
     padding='same',
-	input_shape=(128, 128, 3)))
+	input_shape=(img_height, img_width, img_channels))) #channels_last #first layer in a model, provide input_shape
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2))) #2*2に圧縮
     model.add(Dropout(0.25))
     model.add(Conv2D(64, (3, 3), padding='same'))
     model.add(Activation('relu'))
@@ -51,9 +53,14 @@ def build_model(in_shape):
 
 # モデルを訓練する --- (※3)
 def model_train(X_train, y_train):
-    model = build_model(X_train.shape[1:])
+    model = build_model()
+    #データを与えて学習させる
     model.fit(X_train, y_train, batch_size=32, nb_epoch=30)
-    # モデルを保存する --- (※4)
+    # 学習したモデルを保存する --- (※4)->保存したモデルは再利用できる
+    # モデルを保存
+    model_json_str = model.to_json()
+    open('stock_model.json', 'w').write(model_json_str)
+    # 学習結果を保存
     hdf5_file = "./image/stock-model.hdf5"
     model.save_weights(hdf5_file)
     return model
