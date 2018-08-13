7213c983ce9b72cd5f9dac88e8360367f843f481
katsudonik <23866351+katsudonik@users.noreply.github.com>
2018-08-13 13:49:01 +0900

    .

--- main/stock_keras.py
+++ main/stock_keras.py
@@ -49,7 +49,7 @@ def build_model():
     model.add(Dropout(0.5))
 
     model.add(Dense(len(config['categories']))) #出力層(ノード数：カテゴリ数)
-    model.add(Activation('softmax')) #0-1の確率に正規化
+    model.add(Activation('softmax')) #出力値（分類確率）を0-1に正規化
 
     model.compile(loss='binary_crossentropy', # crossentropy誤差:多クラス分類
 	optimizer='rmsprop', #
